b746f1c191a5871ad466bad25fa4ea664b9b60d1
anirudhkrishna28 <anirudhkrishna.s2021csea@sece.ac.in>
2024-06-23 12:27:27 +0530

    an updated commit message

--- Main.java
+++ Main.java
@@ -2,4 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
     }
+    public void printUser(){
+        User user = new User();
+        user.name = "";
+        System.out.println(user.name);
+
+    }
 }
